dc3c42b2136e4e56d04ae33d6adc9b923a92ad89
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-06-17 23:45:18 -0500

    added cells to the grid

--- javaScript/etch.js
+++ javaScript/etch.js
@@ -3,3 +3,13 @@
 *   June 14, 2019
 *   Etch-A-Sketch 
 */
+
+/*-- ------------------------- Creating grid ---------------------------*/
+const $containerGrid = $(".containerGrid");
+
+for (let i = 0; i < 100; i++) {
+    const $div = $("<div></div>");
+    $($containerGrid).append($div);
+}
+
+
