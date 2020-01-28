530a3ba2007e01e88ac3208b29409052939f90ac
Anthony Gedeon <anthony.gedeonn@gmail.com>
2020-01-28 06:38:01 -0600

    seeing progress

--- javaScript/etch.js
+++ javaScript/etch.js
@@ -1,67 +1,35 @@
 /* 
 *   Anthony Gedeon
-*   August 26, 2019
+*   Jan 28, 2020
 *   Etch-A-Sketch 
 */
 
-/********************
-    Variables
-********************/
-var grid = document.querySelector('.containerGrid');
+class Grid {
+    constructor(row) {
+        const cache = [];
+        this.row = row;
+        this.column = row;
 
-/********************
-    Functions
-*********************/
-var createCell = function() {
-    var cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.append(cell);
-}
-
-var createGrid = function() {
-    var value = parseInt(document.querySelector('#gridLength').value);
-
-    for (var row = 0; row < value; row++) {
-        for (var column = 0; column < value; column++) {
-            createCell();
-        }
     }
 
-    var cells = grid.querySelectorAll('.cell');
-    cells.forEach(function(cell) {
-        cell.style.cssText = `height: ${(300 / value)-1}px; width: ${(300 / value)-1}px;`
-    });
-}
+    static resizeGrid() {
 
-createGrid();
-
-var randomColor = function() {
-    var letters = '0123456789ABCDEF';
-    var hashColor = '#';
-    for (var i = 0; i < 6; i++) {
-        hashColor += letters[Math.floor(Math.random() * letters.length)];
     }
-    return hashColor;
+
+    createGrid() {
+        
+
+    }
 }
 
-var changeColor = function() {
-    var cells = grid.querySelectorAll('.cell');
-
-    cells.forEach(function(cell) {
-        cell.addEventListener('mouseover', function() {
-            cell.style.backgroundColor = randomColor();
-        });
-    });
+class Ui {
+    static createGridCells() {
+        const containerGrid = document.querySelector('.container-grid');
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        containerGrid.appendChild(cell);
+    }
 }
 
-var init = function() {
-    value = document.querySelector('#gridLength').value = 16;
-    createGrid();
-}
-
-/********************
-    Event Listeners
-*********************/
-document.querySelector('#reset').addEventListener('click', init);
-document.querySelector('#random').addEventListener('click', changeColor);
-document.querySelector('#gridLength').addEventListener('click', createGrid);
+// Event Listeners
+document.addEventListener('')
